761bd0b4cc9c2fbbd3e6b79296ec0ee02a88f5be
Priscila Moreno <priscila.moreno.wow@gmail.com>
2018-01-09 12:41:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
-  return "${name} you are the # ${katzDeliLine.indexOf(name) + 1} inline.";
+  return "Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} inline.";
 }
