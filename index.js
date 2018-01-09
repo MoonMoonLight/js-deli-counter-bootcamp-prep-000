ab26d011a8e90843283db5646632ea9afeca6ba6
Priscila Moreno <priscila.moreno.wow@gmail.com>
2018-01-09 12:53:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(katzDeliLine, name){
 
 function nowServing(katzDeliLine, name){
   if(katzDeliLine.length > 1){
-    return `Currently serving ${katzDeliLine[0]}`;
+    return `Currently serving ${katzDeliLine[0]}.`;
     katzDeliLine.shift();
   } else {
     return "There is nobody waiting to be served!"
