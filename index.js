bb4bb4d543ea29da3d89ec9a02708b6d7a29b8f8
Priscila Moreno <priscila.moreno.wow@gmail.com>
2018-01-09 12:51:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`;
 }
+
+function nowServing(katzDeliLine, name){
+  if(katzDeliLine.length > 1){
+    return katzDeliLine[0];
+    katzDeliLine.shift();
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
